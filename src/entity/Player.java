998bce1943e619c897524c0f3341ba7a3c37cfe5
psidbvibsdv <stiltskin44@gmail.com>
2022-12-10 18:34:20 +0200

    test commit

---
+++ src/entity/Player.java
@@ -0,0 +1,125 @@
+package entity;
+
+import java.awt.Graphics2D;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import main.KeyHandler;
+import main.GamePanel;
+
+public class Player extends Entity{
+    
+    GamePanel gp;
+    KeyHandler kh;
+
+    public Player(GamePanel gp, KeyHandler kh){
+
+        this.gp = gp;
+        this.kh = kh;
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues(){
+
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+    public void getPlayerImage(){
+
+        try{
+            up1 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_up_1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_up_2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_down_1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_down_2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_left_1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_left_2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_right_1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/playerpng/masha_right_2.png"));
+        
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void update(){
+
+        if(kh.upPressed == true || kh.downPressed == true || kh.leftPressed == true || kh.rightPressed == true){
+            if(kh.upPressed == true){
+                direction = "up";
+                y -= speed;
+            }
+            else if(kh.downPressed == true){
+                direction = "down";
+                y += speed;
+            }
+            else if(kh.leftPressed == true){
+                direction = "left";
+                x -= speed;
+            }
+            else if(kh.rightPressed == true){
+                direction = "right";
+                x += speed;
+            }
+            spriteCounter++;
+            if (spriteCounter > 15){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }
+                else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }  
+        }
+    }
+
+    public void draw(Graphics2D g2){
+
+       // g2.setColor(Color.white);
+
+        // g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch(direction){
+            case "up":
+            if(spriteNum == 1){
+            image = up1;
+            }
+            if(spriteNum == 2){
+                image = up2;
+                }
+            break;
+            case "down":
+            if(spriteNum == 1){
+                image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                    }
+            break;
+            case "left":
+            if(spriteNum == 1){
+                image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                    }
+            break;
+            case "right":
+            if(spriteNum == 1){
+                image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                    }
+            break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize,gp.tileSize, null);
+    }
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,97 @@
+package main;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+import javax.swing.plaf.DimensionUIResource;
+
+import entity.Player;
+import tile.TileManager;
+
+public class GamePanel extends JPanel implements Runnable{
+    //SCREEN SETTINGS 
+    final int initialTileSize = 16; //16x16px tile  
+    final int scale = 3;
+    
+   public final int tileSize = initialTileSize * scale; //48x48px tile // scaling our tile for an appropriate size in a large resolution
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;    // 768px
+    final int screenHeight = tileSize * maxScreenRow;   // 576px
+
+    //FPS
+    int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
+    KeyHandler kh = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, kh);
+
+
+
+
+    public GamePanel(){
+
+        this.setPreferredSize(new DimensionUIResource(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(kh);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }   
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS; // 0.01666 seconds
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+        double delta = 0; 
+
+        while(gameThread !=null){
+          //  System.out.println("The game is running");
+
+          currentTime = System.nanoTime();
+		
+          delta += (currentTime - lastTime) / drawInterval;
+          timer += (currentTime - lastTime);
+          lastTime = currentTime;
+          
+          if(delta >= 1) {
+              update();
+              repaint();
+              delta--;
+              drawCount++;
+              
+          }
+          
+          if(timer >= 1000000000) {
+              System.out.println("FPS:" + drawCount);
+              drawCount = 0;
+              timer = 0;
+          }
+          
+      }     
+    }
+
+    public void update(){
+        player.update();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+       tileM.draw(g2);
+       player.draw(g2);
+        
+        g2.dispose();
+    }
+
+}
